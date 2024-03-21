bf5226e255fb0ea15ad35a71ad377033549c1def
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-21 15:20:05 +0000

    code update

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,6 +1,7 @@
 --Data manipulation and agregation using sum
+SOURCE metal_bands.sql
 
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
-ORDER BY nb_fans DESC
+ORDER BY nb_fans DESC;
