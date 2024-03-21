f912ebe5ad6b63b7311569ff6bb1ecff475bf55b
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-21 15:06:41 +0000

    update

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,6 @@
+-- enumeration
 CREATE TABLE IF NOT EXISTS users (
-        id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+        id INT PRIMARY KEY AUTO_INCREMENT,
         email VARCHAR(255) NOT NULL UNIQUE,
         name VARCHAR(255),
         country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
