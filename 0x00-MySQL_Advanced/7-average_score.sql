ef91332e26d915486d2cd4bb760f9521407ae6ae
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-23 20:06:21 +0000

    Create first stored procedure

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,1 +1,15 @@
--- create stored procedure
+-- -- create stored procedure 
+
+DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser;
+DELIMITER //
+CREATE PROCEDURE ComputeAverageScoreForUser(
+    IN p_user_id INT
+)
+BEGIN
+    DECLARE avg_score INT;
+    SELECT AVG(score) INTO avg_score FROM corrections WHERE user_id = p_user_id;
+
+    UPDATE users
+    SET average_score = avg_score WHERE id = p_user_id;
+END;
+//
