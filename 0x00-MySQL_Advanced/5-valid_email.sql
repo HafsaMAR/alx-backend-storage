82beab9012435e767a38fde235c6c4a36304448f
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-23 12:32:16 +0000

    update mail and reset valid_email attribute

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,1 +1,13 @@
 -- EMail validation trigger
+DELIMITER $$
+
+CREATE TRIGGER reset_attribute_valid_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email <> NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END;    
+$$
+DELIMITER ;
