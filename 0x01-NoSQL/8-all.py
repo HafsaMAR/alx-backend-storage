5c75e252af8d3e5c95b308330d661447f96a177f
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-25 18:19:36 +0000

    check

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python3
 """Module for listing documents in a collection"""
 
-from pymongo.collection import Collection
-from typing import Iterator
-
 
 def list_all(mongo_collection):
-    """
-    lists all the documents in the collection mongo_collection
+    """lists all the documents in the collection mongo_collection
     Args:
         mongo_collection (pymongo.collection.Collection):
         The MongoDB collection.
 
     Returns:
         Iterator[dict]: An iterator over all the
-        documents in the collection.
-    """
+        documents in the collection."""
     return mongo_collection.find()
