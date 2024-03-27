16b801457b1982d0c9d2ef62fc776791cf1db49b
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-27 11:40:34 +0000

    store Redis data

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,22 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Writing strings to Redis"""
+
+
+import redis
+import uuid
+from typing import Union
+
+class Cache():
+    """Cache class"""
+    def __init__(self) -> None:
+        """Initialize Redis client and flush the db"""
+        self._Redis_client = redis.Redis()
+        self._Redis_client.flushdb()
+
+    def store(self, data: Union[bytes, str, int, float]) -> str:
+        """Generate a random key"""
+        key = str(uuid.uuid4())
+
+        self._Redis_client.set(key, data)
+
+        return key
