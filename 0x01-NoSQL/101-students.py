eb2f41efbe053f23c59dca14ce98c7640af5df42
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-27 13:35:45 +0000

    list of dictionaries agregation

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,26 @@
 #!/usr/bin/env python3
+
+
+from pymongo import MongoClient
+
+
+def top_students(mongo_collection):
+    """
+    Returns all students sorted by average score
+    
+    Args:
+        mongo_collection : pymongo collection object.
+    
+    Returns:
+        list of dictionaries"""
+    pipeline = [
+        {"$unwind": "$scores"},
+        {"$group": {
+            "_id": "$_id",
+            "name": {"$first": "$name"},
+            "averageScore": {"$avg": "$scores.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ]
+
+    return list(mongo_collection.aggregate(pipeline))
