f5de26ea248d638a14ed7fc3c7b61576b2f921ab
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-27 12:37:14 +0000

    Rpush method

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,21 +8,42 @@ import functools
 from typing import Union, Callable, Optional, Any
 
 
+def count_calls(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+def call_history(method: Callable) -> Callable:
+    @functools.wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """construct key for input and output lists"""
+        input_key = method.__qualname__ + ":inputs"
+        output_key = method.__qualname__ + ":outputs"
+        # Append input arguments to the input list
+        self._redis.rpush(input_key, str(args))
+
+        # Execute the wrapper func to retrieve the output
+        output = method(self, *args, **kwargs)
+        self._redis.rpush(output_key, output)
+
+        return output
+
+
+
+    return wrapper   
+
+
 class Cache:
     """Cache class"""
     def __init__(self) -> None:
         """Initialize Redis client and flush the db"""
         self._redis = redis.Redis(host='localhost', port=6379, db=0)
         self._redis.flushdb()
-
-    def count_calls(method: Callable) -> Callable:
-        @functools.wraps(method)
-        def wrapper(self, *args, **kwargs):
-            key = method.__qualname__
-            self._redis.incr(key)
-            return method(self, *args, **kwargs)
-        return wrapper
-
+    
+    @call_history
     @count_calls
     def store(self, data: Union[bytes, str, int, float]) -> str:
         """Generate a random key"""
@@ -50,3 +71,18 @@ class Cache:
     def get_int(self, key: str) -> int:
         """Returns the value stored in the redis store as int"""
         return self.get(key,int)
+
+cache = Cache()
+
+s1 = cache.store("first")
+print(s1)
+s2 = cache.store("secont")
+print(s2)
+s3 = cache.store("third")
+print(s3)
+
+inputs = cache._redis.lrange("{}:inputs".format(cache.store.__qualname__), 0, -1)
+outputs = cache._redis.lrange("{}:outputs".format(cache.store.__qualname__), 0, -1)
+
+print("inputs: {}".format(inputs))
+print("outputs: {}".format(outputs))
