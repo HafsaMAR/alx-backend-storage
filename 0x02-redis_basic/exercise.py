8152d9f06dc5818534a1c8926c72e4c44ccecfe2
HafsaMAR <marouche.hafsa@gmail.com>
2024-03-27 12:23:06 +0000

    implement the count_calls decorator and decorate the Cache.store method with it

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -4,6 +4,7 @@
 
 import redis
 import uuid
+import functools
 from typing import Union, Callable, Optional, Any
 
 
@@ -11,18 +12,28 @@ class Cache:
     """Cache class"""
     def __init__(self) -> None:
         """Initialize Redis client and flush the db"""
-        self._Redis_client = redis.Redis(host='localhost', port=6379, db=0)
-        self._Redis_client.flushdb()
+        self._redis = redis.Redis(host='localhost', port=6379, db=0)
+        self._redis.flushdb()
 
+    def count_calls(method: Callable) -> Callable:
+        @functools.wraps(method)
+        def wrapper(self, *args, **kwargs):
+            key = method.__qualname__
+            self._redis.incr(key)
+            return method(self, *args, **kwargs)
+        return wrapper
+
+    @count_calls
     def store(self, data: Union[bytes, str, int, float]) -> str:
         """Generate a random key"""
         key = str(uuid.uuid4())
-        self._Redis_client.set(key, data)
+        self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Optional[Callable[[Any],Any]] = None) -> Any:
+    def get(self, key: str,
+            fn: Optional[Callable[[Any],Any]] = None) -> Any:
         """Retrieve data from Redis using the provided key"""
-        data = self._Redis_client.get(key)
+        data = self._redis.get(key)
 
         if data is None:
             return None
